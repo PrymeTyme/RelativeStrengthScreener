6c00a5ffcab8dd9f7294cc817593ec70a2e2de94
PrymeTyme <orasch.s@live.at>
2022-09-29 17:15:43 +0200

    bugfix in stores

--- src/stores/tickers.js
+++ src/stores/tickers.js
@@ -1,17 +1,41 @@
 import { defineStore } from "pinia";
+//import { reactive } from "vue";
+import { ref } from "vue";
 
-export const useTickerStore = defineStore({
-    id:'tickers',
-    state:()=>({
-        ticker:""
+
+/* export const useTickerStore = defineStore(
+    'ticker', () =>{
+        var ticker = ref("");
+
+        const getTicker = (index) =>{
+            var element = document.getElementsByClassName('listItem');
+            var tickerZ = element[index].firstChild.data;
+            console.log(tickerZ);  
+            ticker = tickerZ  
+            
+
+        }
+
+        return {ticker,getTicker};
+    }
+) */
+
+ export const useTickerStore = defineStore({
+    id: 'tickers',
+    state: () => ({
+        ticker: ref("")
+
 
     }),
 
-    actions:{
+    actions: {
         getTicker(index) {
             var element = document.getElementsByClassName('listItem');
             var ticker = element[index].firstChild.data;
-            console.log(ticker)
-            this.ticker = ticker;
-    }}
-})
+            ticker.trim();
+            console.log(ticker);
+            console.log(typeof ticker);
+            return this.ticker = ticker;
+        }
+    }
+}) 

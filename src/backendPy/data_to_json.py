50bec1fc786b1fa0874d5fc91b4fc03aaaff6a9c
PrymeTyme <orasch.s@live.at>
2022-10-17 11:21:30 +0200

    needed to rewrite algo function so it works with json files

--- src/backendPy/data_to_json.py
+++ src/backendPy/data_to_json.py
@@ -10,8 +10,8 @@ def save_stock_dict_to_json(stock_dict):
         v.to_json('C:\\Users\\Sergej\\hello-world\\data\\stocks\\'+str(k)+'.json') # change to format
 
 def save_sectors_to_json(sectors):
-    sectors.to_json('C:\\Users\\Sergej\\hello-world\\data\\sectors\\sectors.json')        
-
+    sectors.to_json('C:\\Users\\Sergej\\hello-world\\data\\sectors\\sectors.json')
+      
 # sectors_df.to_json(r'C:\Users\Sergej\hello-world\src\jsonData\sectors.json')
 # xle_df.to_json(r'C:\Users\Sergej\hello-world\src\jsonData\xle_df.json')
 # xlk_df.to_json(r'C:\Users\Sergej\hello-world\src\jsonData\xlx_df.json')
--- src/backendPy/relative_strength_algo.py
+++ src/backendPy/relative_strength_algo.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import json
+#from fetch_api_data import sectors_data, ticker_data_list
 
 
 
@@ -17,38 +19,43 @@ def rsc_algo_test(dataframe,timeframe):
     return sorted_symbols
 
 
-
-#daily = slice(0,2)
-#weekly = slice(0,6)
-#monthly = slice(0,31)
-#yearly = slice(0,366)
-
-
-
-#run the rsc algo on sectors
-
-#sector_daily = rsc_algo_test(sectors_df,daily) 
-#sector_weekly = rsc_algo_test(sectors_df,weekly)
-#sector_monthly = rsc_algo_test(sectors_df,monthly) 
-#sector_yearly = rsc_algo_test(sectors_df,yearly)
-
-
-
-# algo ouput into dataframe 
-
-#sector_daily_df =  pd.DataFrame(sector_daily,columns=['Ticker','RSC','Price','Percent Change','Index'])
-#sector_weekly_df =  pd.DataFrame(sector_weekly,columns=['Ticker','RSC','Price','Percent Change','Index'])
-#sector_monthly_df =  pd.DataFrame(sector_monthly,columns=['Ticker','RSC','Price','Percent Change','Index'])
-#sector_yearly_df =  pd.DataFrame(sector_yearly,columns=['Ticker','RSC','Price','Percent Change','Index'])
-
-#sector_daily_df.to_json(r'C:\Users\Sergej\hello-world\src\jsonData\sector_daily_df.json')
-#sector_daily_df.to_json(r'C:\Users\Sergej\hello-world\data\sector_daily_df_test.json')
-#sector_weekly_df.to_json(r'C:\Users\Sergej\hello-world\data\sector_weekly_df_test.json')
-#sector_monthly_df.to_json(r'C:\Users\Sergej\hello-world\data\sector_monthly_df_test.json')
-#sector_yearly_df.to_json(r'C:\Users\Sergej\hello-world\data\sector_yearly_df_test.json')
+def rsc_algo(dataframe,timeframe): ## when using json files i meed to reverse the columns values order
+    dataframe = dataframe.iloc[::-1]  ## when using json files i meed to reverse the columns values order
+    columns = list(dataframe)
+    base_instrument = dataframe.columns.values[-1]
+    sorted_symbols=[]
+    
+    for column in columns:
+        if column == base_instrument:
+            continue
+        sorted_symbols.append(tuple((column,(dataframe[column][timeframe] / dataframe[base_instrument][timeframe])[::-1].pct_change().sum(),dataframe[column][0],
+                                     round((dataframe[column][timeframe][::-1].pct_change().sum())*100,2),base_instrument))) # output tuple with ticker,rsc
+        
+        sorted_symbols.sort( key=lambda x: x[1],reverse=True)
+    return sorted_symbols
 
 
+daily = slice(0,2)
+weekly = slice(0,6)
+monthly = slice(0,31)
+yearly = slice(0,366)
 
+timeframes = [daily,weekly,monthly,yearly]
+string_timeframes = ['daily','weekly','monthly','yearly']
+
+with open(r'C:\Users\Sergej\hello-world\data\sectors\sectors.json') as f:
+    sector_data = json.load(f)
+    sector_data = pd.DataFrame.from_dict(sector_data)
+
+count = -1   #horrible way of doing it but it works... ;) (refactor later... XD)
+
+for i in timeframes:
+    sector="sector"
+    x = rsc_algo(sector_data,i)
+    y = pd.DataFrame(x, columns=['Ticker', 'RSC', 'Price', 'Percent Change', 'Index'])
+    count +=1 
+    timeframe=string_timeframes[count]
+    y.to_json(fr'C:\Users\Sergej\hello-world\data\sectorSorted\{timeframe}\{sector}_{timeframe}.json')
 
 
 # Stock Tickers rsc algo > to dataframe

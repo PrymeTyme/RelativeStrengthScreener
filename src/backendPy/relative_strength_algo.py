f6d54bca7699d9b17b669e8ac9605c209c637757
PrymeTyme <orasch.s@live.at>
2022-10-17 12:45:34 +0200

    added stocks sorted to backend json database

--- src/backendPy/relative_strength_algo.py
+++ src/backendPy/relative_strength_algo.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import json
+import os
+import glob
 #from fetch_api_data import sectors_data, ticker_data_list
 
 
@@ -43,22 +45,42 @@ yearly = slice(0,366)
 timeframes = [daily,weekly,monthly,yearly]
 string_timeframes = ['daily','weekly','monthly','yearly']
 
-with open(r'C:\Users\Sergej\hello-world\data\sectors\sectors.json') as f:
-    sector_data = json.load(f)
-    sector_data = pd.DataFrame.from_dict(sector_data)
+#with open(r'C:\Users\Sergej\hello-world\data\sectors\sectors.json') as f:
+ #   sector_data = json.load(f)
+  #  sector_data = pd.DataFrame.from_dict(sector_data)
 
 count = -1   #horrible way of doing it but it works... ;) (refactor later... XD)
 
-for i in timeframes:
-    sector="sector"
-    x = rsc_algo(sector_data,i)
-    y = pd.DataFrame(x, columns=['Ticker', 'RSC', 'Price', 'Percent Change', 'Index'])
-    count +=1 
-    timeframe=string_timeframes[count]
-    y.to_json(fr'C:\Users\Sergej\hello-world\data\sectorSorted\{timeframe}\{sector}_{timeframe}.json')
+#for i in timeframes:
+ #   sector="sector"
+  #  x = rsc_algo(sector_data,i)
+   # y = pd.DataFrame(x, columns=['Ticker', 'RSC', 'Price', 'Percent Change', 'Index'])
+    #count +=1 
+    #timeframe=string_timeframes[count]
+    #y.to_json(fr'C:\Users\Sergej\hello-world\data\sectorSorted\{timeframe}\{sector}_{timeframe}.json')
 
 
-# Stock Tickers rsc algo > to dataframe
+# Stock Tickers rsc algo > to dataframe > to json
+
+for filepath in glob.glob(os.path.join(r'C:\Users\Sergej\hello-world\data\stocks','*.json')):
+    with open(filepath) as f:
+        data = json.load(f)
+        data = pd.DataFrame.from_dict(data)
+
+        for i in timeframes:
+            x = rsc_algo(data,i)
+            y = pd.DataFrame(x, columns=['Ticker', 'RSC', 'Price', 'Percent Change', 'Index'])
+            index = y.loc[0,'Index']
+            count += 1
+            timeframe=string_timeframes[count]
+            y.to_json(fr'C:\Users\Sergej\hello-world\data\stockSorted\{timeframe}\{index}_{timeframe}.json')
+
+            #print(timeframe +': '+str(y)+' Index = '+index)
+            if count >= 3 :   #horrible hard coded  but works ;) .. refactor later XD haha !
+                count = -1
+
+
+
 
 
 

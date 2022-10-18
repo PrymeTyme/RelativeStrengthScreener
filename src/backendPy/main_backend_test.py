7741aa0dc687c395238935f41fba668f3071f23c
PrymeTyme <orasch.s@live.at>
2022-10-18 14:35:17 +0200

    working on threading await

--- src/backendPy/main_backend_test.py
+++ src/backendPy/main_backend_test.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
 #1st  get actual ticker names from spdrwebsite , then save as backend
     #if error get ticker list from backup
-    stock_tickers =  stock_tickers_final
+    stock_tickers =  stock_tickers_final # wait till finished
 
 #2nd fetch from tiingo api
 #  if  error , skip and use from json
@@ -22,18 +22,18 @@ if __name__ == '__main__':
 
     sectors = ['XLE','XLU','XLK','XLB','XLP','XLY','XLI','XLC','XLV','XLF','XLRE','SPY']
 
-    sectors_data = fetch_tickers(sectors,start_date,end_date) # raw ticker data
+    sectors_data = fetch_tickers(sectors,start_date,end_date) # raw ticker data  # wait till finished
 
-    xle_df,xlu_df,xlk_df,xlb_df,xlp_df,xly_df,xli_df,xlc_df,xlv_df,xlf_df,xlre_df = (fetch_tickers(x,start_date,end_date) for x in stock_tickers) 
+    xle_df,xlu_df,xlk_df,xlb_df,xlp_df,xly_df,xli_df,xlc_df,xlv_df,xlf_df,xlre_df = (fetch_tickers(x,start_date,end_date) for x in stock_tickers)  # wait
 
-    ticker_data_list = [xle_df,xlu_df,xlk_df,xlb_df,xlp_df,xly_df,xli_df,xlc_df,xlv_df,xlf_df,xlre_df] # raw ticker data
+    ticker_data_list = [xle_df,xlu_df,xlk_df,xlb_df,xlp_df,xly_df,xli_df,xlc_df,xlv_df,xlf_df,xlre_df] # raw ticker data # wait till finished
  
 # 
 # 3rd save raw ticker data (for charts) in json data
-    stock_dict = dict(zip(sector_stock_list,ticker_data_list))
+    stock_dict = dict(zip(sector_stock_list,ticker_data_list)) #wait
 
-    save_stock_dict_to_json(stock_dict)
-    save_sectors_to_json(sectors_data)
+    save_stock_dict_to_json(stock_dict) # wait
+    save_sectors_to_json(sectors_data) # wait
 
     algo_data_to_json
 # 4th run rsc algo on sectors

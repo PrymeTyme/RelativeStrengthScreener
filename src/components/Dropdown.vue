31e3aa155ffc1d1886e5c581a715a1b5139651e3
PrymeTyme <orasch.s@live.at>
2022-10-25 15:12:34 +0200

    changed border radius on dropdown items

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -44,8 +44,8 @@ nav .menu-item .sub-menu {
     transform: translateX(-50%);
     width: max-content;
     z-index: 5;
-    border-bottom-left-radius: 5%;
-    border-bottom-right-radius: 5%;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
 
 }
 
